5ad21da9b8562645f627385e2fa4cdff382bff2d
sowmy <sowmy@192.168.0.5>
2021-03-08 15:08:12 +0530

    first commit

---
+++ addition/src/addition/example.java
@@ -0,0 +1,19 @@
+package addition;
+import java.util.Scanner;
+public class example {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int i;
+	int[] a= {1,2,3,4,5,6,7,8};
+    int[] b= {a.length};
+    b=a;
+    for(i=0;i<a.length;i++)
+    {
+    	System.out.println(b[i]);
+    }
+
+	}
+}
+
+	
---
+++ addition/src/house/hall.java
@@ -0,0 +1,11 @@
+package house;
+import java.util.*;
+public class hall {
+	static int id=1;
+	
+public static void main(String[] args)
+{
+	System.out.println(id);
+ 
+}
+}
